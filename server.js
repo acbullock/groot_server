abc00a3db5cdf47fb6ac12c6f6bbfff7f733d3f9
Alexander Bullock <alexanderbullock@Alexanders-MBP.attlocal.net>
2024-11-24 12:04:49 -0500

    basic server

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const authRoutes = require('./routes/authRoutes');
 const phoneNumberRoutes = require('./routes/phoneNumberRoutes');
 // Sample protected route (replace with your routes)
 const authenticateJWT = require('./middleware/auth');
+// Folder containing files
+const folderPath = path.join(__dirname, 'box_files');
 
 app.use(cors());
 // app.use(cors({
@@ -22,9 +24,45 @@ app.use('/api/auth', authRoutes);
 app.get('/admin', authenticateJWT(['admin']), (req, res) => {
   res.json({ message: 'Welcome Admin!' });
 });
+// GET endpoint to list files containing "date" in the filename
+app.get('/files', (req, res) => {
+  const { date } = req.query;
 
-app.use('/teliance-web', express.static(path.join(__dirname, '../teliance-web/build')))
-app.get('/teliance-web/*', (req, res) => res.sendFile(path.join(__dirname, '../teliance-web/build', 'index.html')))
+  if (!date) {
+    return res.status(400).json({ error: 'Query parameter "date" is required' });
+  }
+
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: 'Error reading the directory', err });
+    }
+
+    // Filter files that contain the date string
+    const matchingFiles = files.filter(file => file.includes(date)).map(file => `http://localhost:3000/file?filename=${file}`)
+    res.json({ files: matchingFiles });
+  });
+});
+// GET endpoint to return the file file
+app.get('/file', (req, res) => {
+  const { filename } = req.query;
+
+  if (!filename) {
+    return res.status(400).json({ error: 'Query parameter "filename" is required' });
+  }
+
+  const filePath = path.join(folderPath, filename);
+
+  // Check if the file exists
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      return res.status(404).json({ error: 'File not found' });
+    }
+
+    // Send the file
+    res.sendFile(filePath);
+  });
+});
+
 // Load SSL certificate and key
 const options = {
   key: fs.readFileSync('/root/telianceDB/teliance.key'),  // Replace with the actual path to your private key
@@ -55,5 +93,4 @@ https.createServer(options, app).listen(HTTPS_PORT, '0.0.0.0', () => {
 // }).listen(HTTP_PORT, '0.0.0.0', () => {
 //   console.log(`HTTP Server running, redirecting all traffic to https://localhost:${HTTPS_PORT}`);
 // });
-
 
